3249dcffd37b4c94816d59c36c18205b018e45a9
Toni Nguyen <toninguyen2310@gmail.com>
2025-08-09 05:13:32 +0700

    fix vercel remix file

--- remix.config.js
+++ remix.config.js
@@ -1,18 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+    serverBuildTarget: "vercel",
+    server: "./server.js", // hoặc server.ts tùy bạn
     ignoredRouteFiles: ["**/.*"],
-    serverModuleFormat: "cjs",
-    serverPlatform: "node",
-    serverMinify: process.env.NODE_ENV === "production",
-    serverDependenciesToBundle: [
-        "@shopify/shopify-app-remix",
-        /^@shopify\/shopify-app-session-storage.*/,
-    ],
-    future: {
-        v2_errorBoundary: true,
-        v2_headers: true,
-        v2_meta: true,
-        v2_normalizeFormMethod: true,
-        v2_routeConvention: true,
-    },
 };
